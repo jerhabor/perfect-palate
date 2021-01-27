5dc73c4e47727f1b0ba79d9759930b267424e3e2
Jesse Erhabor <jess.erhabor@gmail.com>
2021-01-27 15:30:19 +0000

    FEAT: Added the Login page and functionality which checks whether the user's input exists in the database.

--- app.py
+++ app.py
@@ -58,6 +58,35 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Checking if username already exists in the DB as lowercase:
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Checking if the hashed password matches the user's input:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hello, {}".format(request.form.get("username")))
+            else:
+                # If the invalid password is incorrect then alert user.
+                # For security reasons message displays either username
+                # or password is incorrect. This minimises brute-forcing.
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # If the username does not exist then display the same message
+            # as if the password was incorrect.
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
