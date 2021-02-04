d0a681ac61b1f18e600622e44d8d168667a595f6
Jesse Erhabor <jess.erhabor@gmail.com>
2021-02-04 14:39:08 +0000

    FIX/FEAT: Added a custom validation in jquery on the is_vegetarian select form on the new_recipe page. This ensures a value is given prior to submission and alerts the user as a reminder.

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         $(".method_steps").append(
             `<li>
                 <textarea id="method" name="method" type="text" class="validate materialize-textarea" required></textarea>
-                <label for="method">Delete Step <a href='javascript:void(0);' class='remove_step'>&times;</a></label>
+                <label for="method" class="active">Delete Step <a href='javascript:void(0);' class='remove_step'>&times;</a></label>
             </li>`
         );
         $("#first_method").empty();
@@ -23,11 +23,11 @@ $(document).ready(function() {
             `<li>
                 <div class="input-field col s6 m8">
                     <input id="ingredient_name" name="ingredient_name" minlength="5" maxlength="50" type="text" class="validate" required>
-                    <label for="ingredient_name">Ingredient Name <em>(e.g. Ripe Plantain)</em></label>
+                    <label for="ingredient_name" class="active">Ingredient Name <em>(e.g. Ripe Plantain)</em></label>
                 </div>
                 <div class="input-field col s5 m3">
                     <input id="ingredient_amount" name="ingredient_amount" minlength="5" maxlength="50" type="text" class="validate" required>
-                    <label for="ingredient_amount">Amount <em>(e.g. 3 tablespoons)</em></label>
+                    <label for="ingredient_amount" class="active">Amount <em>(e.g. 3 tablespoons)</em></label>
                 </div>
                 <div class="input-field col s1 m1">
                     <button class='remove_ingredient red'><i class="fa fa-times"></i></button>
@@ -48,4 +48,34 @@ $(document).ready(function() {
             $(this).parent().parent().remove();
         };
     });
+
+
+    // Validation of the 'is_Vegetarian' select (Code inspired by Tim Nelson, Code Institute)
+    validateIsVegetarian();
+    function validateIsVegetarian() {
+        let validInput = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let invalidInput = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(validInput);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(validInput);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(invalidInput);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
